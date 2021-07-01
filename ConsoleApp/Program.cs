7b5207b0dab039925dda2c36944fb04c3ff6a093
Farooq Mahmud <farooq.mahmud@marel.com>
2021-07-01 00:53:33 -0700

    First.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+        }
+    }
+}
