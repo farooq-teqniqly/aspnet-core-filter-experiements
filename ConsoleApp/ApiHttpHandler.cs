32d66234ea57f93ce97409db3a8e5d91533218f6
Farooq Mahmud <farooq.mahmud@marel.com>
2021-07-01 15:48:07 -0700

    wip

---
+++ ConsoleApp/ApiHttpHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class ApiHttpHandler : DelegatingHandler
+    {
+        private readonly string token;
+
+        public ApiHttpHandler(string token)
+        {
+            this.token = token;
+            this.InnerHandler = new HttpClientHandler();
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authHeader = request.Headers.Authorization;
+
+            if (authHeader == null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", this.token);
+            }
+
+            var accept = request.Headers.Accept;
+
+            if (accept == null || accept.All(m => m.MediaType != "application/json"))
+            {
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
 
 namespace ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(System.IO.Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+            var appClientId = config["ConsoleApp:ClientId"];
+            var appClientSecret = config["ConsoleApp:Secret"];
+            var appAuthority = $"{config["ConsoleApp:Instance"]}/{config["ConsoleApp:TenantId"]}";
+
+            var app = ConfidentialClientApplicationBuilder
+                .Create(appClientId)
+                .WithClientSecret(appClientSecret)
+                .WithAuthority(appAuthority)
+                .Build();
+
+            var authResult = await app
+                .AcquireTokenForClient(new[] {config["Api:Scope"]})
+                .ExecuteAsync();
+
+            var token = authResult.AccessToken;
+
+            Console.WriteLine(token);
+
+            //var http = new HttpClient(new ApiHttpHandler(token))
+            //{
+            //    BaseAddress = new Uri(config["Api:BaseUri"])
+            //};
+
+            //using (http)
+            //{
+            //    var response = await http.GetAsync($"/weatherforecast");
+            //    var content = await response.Content.ReadAsStringAsync();
+            //    Console.WriteLine(content);
+            //}
         }
     }
 }
